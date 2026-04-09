584cbca7a1780b05ca78c2200977db0234b418dc
Antonio Elias <aelias12j@gmail.com>
2026-04-08 22:10:08 -0300

    feat : adicionando notificador

---
+++ src/FitAgenda.Domain/Interfaces/INotificador.cs
@@ -0,0 +1,12 @@
+﻿
+using FitAgenda.Domain.Notifications;
+
+namespace FitAgenda.Domain.Interfaces
+{
+    public interface INotificador
+    {
+        void AdicionarNotificacao(Notificacao notificacao);
+        List<Notificacao> ObterNotificacoes();
+        bool TemNotificacao();
+    }
+}
---
+++ src/FitAgenda.Domain/Notifications/Notificador.cs
@@ -0,0 +1,20 @@
+﻿using FitAgenda.Domain.Interfaces;
+using FitAgenda.Domain.Notifications;
+
+namespace FitAgenda.Domain.Notifications;
+
+public class Notificador : INotificador
+{
+    private readonly List<Notificacao> _notificacoes = new();
+
+    public void AdicionarNotificacao(Notificacao notificacao)
+    {
+        _notificacoes.Add(notificacao);
+    }
+
+    public List<Notificacao> ObterNotificacoes()
+        => _notificacoes;
+
+    public bool TemNotificacao()
+        => _notificacoes.Any();
+}
