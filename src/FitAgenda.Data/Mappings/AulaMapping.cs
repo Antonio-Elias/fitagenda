036bba87f1154dcccd7df36b811571d1e2d2e794
Antonio Elias <aelias12j@gmail.com>
2026-04-09 00:52:07 -0300

    feat : mapeando entidade aula

---
+++ src/FitAgenda.Data/Mappings/AulaMapping.cs
@@ -0,0 +1,28 @@
+﻿using FitAgenda.Domain.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace FitAgenda.Data.Mappings;
+
+public class AulaMapping : IEntityTypeConfiguration<Aula>
+{
+    public void Configure(EntityTypeBuilder<Aula> builder)
+    {
+        builder.ToTable("aulas");
+
+        builder.HasKey(x => x.Id);
+
+        builder.Property(x => x.CapacidadeMaxima)
+            .IsRequired();
+
+        builder.Property(x => x.DataHora)
+            .IsRequired();
+
+        builder.Property(x => x.Ativa)
+            .IsRequired();
+
+        builder.HasOne(x => x.TipoAula)
+            .WithMany(t => t.Aulas)
+            .HasForeignKey(x => x.CodigoTipoAula);
+    }
+}
---
+++ src/FitAgenda.Data/Mappings/TipoAulaMapping.cs
@@ -0,0 +1,22 @@
+﻿using FitAgenda.Domain.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace FitAgenda.Data.Mappings;
+
+public class TipoAulaMapping : IEntityTypeConfiguration<TipoAula>
+{
+    public void Configure(EntityTypeBuilder<TipoAula> builder)
+    {
+        builder.ToTable("tipoaula");
+
+        builder.HasKey(x => x.Id);
+
+        builder.Property(x => x.Nome)
+            .IsRequired()
+            .HasMaxLength(150);
+
+        builder.Property(x => x.Ativo)
+            .IsRequired();
+    }
+}
