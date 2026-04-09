fb745a5121e7bb46c1f67c938173bc723a65ec8d
Antonio Elias <aelias12j@gmail.com>
2026-04-09 01:01:24 -0300

    feat : preparando o aplicação para trabalhar com o banco postgres

--- src/FitAgenda.Api/Program.cs
+++ src/FitAgenda.Api/Program.cs
@@ -1,8 +1,12 @@
+using FitAgenda.Data.Context;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
-
-
 builder.Services.AddControllers();
+
+builder.Services.AddDbContext<AppDbContext>(options =>
+    options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 builder.Services.AddOpenApi();
 
