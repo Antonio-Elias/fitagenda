80d7d727b7741606dc7c29346b5f0a0d56553042
Antonio Elias <aelias12j@gmail.com>
2026-04-09 00:20:27 -0300

    feat : implementando consulta a dados para  aula.

---
+++ src/FitAgenda.Data/Repositories/AulaRepository.cs
@@ -0,0 +1,27 @@
+﻿using FitAgenda.Data.Context;
+using FitAgenda.Domain.Interfaces;
+using FitAgenda.Domain.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace FitAgenda.Data.Repositories;
+
+public class AulaRepository : Repository<Aula>, IAulaRepository
+{
+    public AulaRepository(AppDbContext context) : base(context)
+    {
+    }
+
+    public async Task<Aula?> ObterAulaComTipo(Guid codigoAula)
+    {
+        return await Db.Aulas
+            .Include(aula => aula.TipoAula)
+            .FirstOrDefaultAsync(aula => aula.Id == codigoAula);
+    }
+
+    public async Task<List<Aula>> ObterAulasComTipo()
+    {
+        return await Db.Aulas
+            .Include(aula => aula.TipoAula)
+            .ToListAsync();
+    }
+}
---
+++ src/FitAgenda.Domain/Interfaces/IAulaRepository.cs
@@ -0,0 +1,9 @@
+﻿using FitAgenda.Domain.Models;
+
+namespace FitAgenda.Domain.Interfaces;
+
+public interface IAulaRepository : IRepository<Aula>
+{
+    Task<Aula?> ObterAulaComTipo(Guid codigoAula);
+    Task<List<Aula>> ObterAulasComTipo();
+}
