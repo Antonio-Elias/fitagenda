705c729422709619976bf5d5a5e599c65337a780
Antonio Elias <aelias12j@gmail.com>
2026-04-08 21:46:15 -0300

    feat: adiciona regra para total de aulas por tipo de plano

---
+++ src/FitAgenda.Domain/Rules/TipoPlanoRules.cs
@@ -0,0 +1,18 @@
+using FitAgenda.Domain.Enums;
+
+namespace FitAgenda.Domain.Rules
+{
+	public static class TipoPlanoRules
+	{
+		public static int ObterLimitePlano(TipoPlano plano)
+		{
+			return plano switch
+			{
+				TipoPlano.Mensal => 12,
+				TipoPlano.Trimestral => 20,
+				TipoPlano.Anual => 30,
+				_ => throw new ArgumentOutOfRangeException(nameof(plano), "Tipo de plano desconhecido.")
+			};
+		}
+	}
+}
