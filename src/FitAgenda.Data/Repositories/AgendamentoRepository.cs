f35415449c4c3ffa0c5c4be5de25ef3fec2bbec9
Antonio Elias <aelias12j@gmail.com>
2026-04-08 23:59:32 -0300

    feat  : implementando os metodos iniciais de consulta a dados do agendamento, especializacao.

---
+++ src/FitAgenda.Data/Repositories/AgendamentoRepository.cs
@@ -0,0 +1,48 @@
+﻿using FitAgenda.Data.Context;
+using FitAgenda.Domain.Interfaces;
+using FitAgenda.Domain.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace FitAgenda.Data.Repositories
+{
+    public class AgendamentoRepository : Repository<Agendamento>, IAgendamentoRepository
+    {
+        public AgendamentoRepository(AppDbContext context) : base(context)
+        {
+        }
+        public async Task<List<Agendamento>> ObterTodosComDetalhes()
+        {
+            return await Db.Agendamentos
+                .Include(agendamento => agendamento.Aluno)
+                .Include(agendamento => agendamento.Aula)
+                    .ThenInclude(aula => aula.TipoAula)
+                .ToListAsync();
+        }
+
+        public async Task<Agendamento?> ObterPorIdComDetalhes(Guid codigoAgendamento)
+        {
+            return await Db.Agendamentos
+                .Include(agendamento => agendamento.Aluno)
+                .Include(agendamento => agendamento.Aula)
+                    .ThenInclude(aula => aula.TipoAula)
+                .FirstOrDefaultAsync(agendamento => agendamento.Id == codigoAgendamento);
+        }
+
+        public async Task<Aluno?> ObterAlunoComAgendamentos(Guid codigoAluno)
+        {
+            return await Db.Alunos
+                .Include(aluno => aluno.Agendamentos)
+                    .ThenInclude(agendamento => agendamento.Aula)
+                        .ThenInclude(aula => aula.TipoAula)
+                .FirstOrDefaultAsync(aluno => aluno.Id == codigoAluno);
+        }
+
+        public async Task<Aula?> ObterAulaComAgendamentos(Guid codigoAula)
+        {
+            return await Db.Aulas
+                .Include(aula => aula.Agendamentos)
+                .Include(aula => aula.TipoAula)
+                .FirstOrDefaultAsync(aula => aula.Id == codigoAula);
+        }
+    }
+}
---
+++ src/FitAgenda.Domain/Interfaces/IAgendamentoRepository.cs
@@ -0,0 +1,11 @@
+﻿using FitAgenda.Domain.Models;
+
+namespace FitAgenda.Domain.Interfaces;
+
+public interface IAgendamentoRepository : IRepository<Agendamento>
+{
+    Task<List<Agendamento>> ObterTodosComDetalhes();
+    Task<Agendamento?> ObterPorIdComDetalhes(Guid id);
+    Task<Aluno?> ObterAlunoComAgendamentos(Guid codigoAluno);
+    Task<Aula?> ObterAulaComAgendamentos(Guid codigoAula);
+}
