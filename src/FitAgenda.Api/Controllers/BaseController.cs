fd5787cb27101ab4693853dc6e1f02c1d59f528a
Antonio Elias <aelias12j@gmail.com>
2026-04-09 02:02:40 -0300

    feat : implementando a base controller

---
+++ src/FitAgenda.Api/Controllers/BaseController.cs
@@ -0,0 +1,75 @@
+using FitAgenda.Domain.Interfaces;
+using FitAgenda.Domain.Notifications;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.ModelBinding;
+using System.Net;
+
+namespace FitAgenda.Api.Controllers;
+
+[ApiController]
+public abstract class BaseController : ControllerBase
+{
+    private readonly INotificador _notificador;
+
+    protected BaseController(INotificador notificador)
+    {
+        _notificador = notificador;
+    }
+
+    protected bool OperacaoValida()
+    {
+        return !_notificador.TemNotificacao();
+    }
+
+    protected ActionResult CustomResponse(HttpStatusCode statusCode = HttpStatusCode.OK, object? result = null)
+    {
+        if (OperacaoValida())
+        {
+            return new ObjectResult(new
+            {
+                sucesso = true,
+                dados = result
+            })
+            {
+                StatusCode = (int)statusCode
+            };
+        }
+
+        var errorStatusCode = (int)statusCode < 400
+            ? HttpStatusCode.BadRequest
+            : statusCode;
+
+        return new ObjectResult(new
+        {
+            sucesso = false,
+            erros = _notificador.ObterNotificacoes().Select(notificacao => notificacao.Mensagem)
+        })
+        {
+            StatusCode = (int)errorStatusCode
+        };
+    }
+
+    protected ActionResult CustomResponse(ModelStateDictionary modelState)
+    {
+        if (!modelState.IsValid)
+            NotificarErroModelInvalida(modelState);
+
+        return CustomResponse(HttpStatusCode.BadRequest);
+    }
+
+    protected void NotificarErroModelInvalida(ModelStateDictionary modelState)
+    {
+        var erros = modelState.Values.SelectMany(entrada => entrada.Errors);
+
+        foreach (var erro in erros)
+        {
+            var mensagem = erro.Exception == null ? erro.ErrorMessage : erro.Exception.Message;
+            NotificarErro(mensagem);
+        }
+    }
+
+    protected void NotificarErro(string mensagem)
+    {
+        _notificador.AdicionarNotificacao(new Notificacao(mensagem));
+    }
+}
