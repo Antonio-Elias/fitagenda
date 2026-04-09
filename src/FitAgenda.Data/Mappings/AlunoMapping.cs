268348689ea539f668907b05e1029035e069f3f4
Antonio Elias <aelias12j@gmail.com>
2026-04-09 00:48:11 -0300

    feat : mapeando a entidade aluno

---
+++ src/FitAgenda.Data/Mappings/AlunoMapping.cs
@@ -0,0 +1,26 @@
+﻿
+using FitAgenda.Domain.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace FitAgenda.Data.Mappings;
+
+public class AlunoMapping : IEntityTypeConfiguration<Aluno>
+{
+    public void Configure(EntityTypeBuilder<Aluno> builder)
+    {
+        builder.ToTable("alunos");
+
+        builder.HasKey(x => x.Id);
+
+        builder.Property(x => x.Nome)
+            .IsRequired()
+            .HasMaxLength(150);
+
+        builder.Property(x => x.TipoPlano)
+            .IsRequired();
+
+        builder.Property(x => x.Ativo)
+            .IsRequired();
+    }
+}
