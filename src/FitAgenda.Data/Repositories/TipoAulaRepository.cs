b3f332a57e13925ae16154b3f086443cb3e17bfa
Antonio Elias <aelias12j@gmail.com>
2026-04-09 00:24:10 -0300

    feat : implementando consulta a dados para o tipo aula, acesso aos metodos padrões

---
+++ src/FitAgenda.Data/Repositories/TipoAulaRepository.cs
@@ -0,0 +1,12 @@
+﻿using FitAgenda.Data.Context;
+using FitAgenda.Domain.Interfaces;
+using FitAgenda.Domain.Models;
+
+namespace FitAgenda.Data.Repositories;
+
+public class TipoAulaRepository : Repository<TipoAula>, ITipoAulaRepository
+{
+    public TipoAulaRepository(AppDbContext context) : base(context)
+    {
+    }
+}
---
+++ src/FitAgenda.Domain/Interfaces/ITipoAulaRepository.cs
@@ -0,0 +1,7 @@
+﻿using FitAgenda.Domain.Models;
+
+namespace FitAgenda.Domain.Interfaces;
+
+public interface ITipoAulaRepository : IRepository<TipoAula>
+{
+}
