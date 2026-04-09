bad5477ce621cea9aba4943da35ada29a396f768
Antonio Elias <aelias12j@gmail.com>
2026-04-09 00:44:03 -0300

    feat : mapeando a entidade agendamentos no banco, instalado componente do postgres.

---
+++ src/FitAgenda.Data/Mappings/AgendamentoMapping.cs
@@ -0,0 +1,29 @@
+﻿using FitAgenda.Domain.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace FitAgenda.Data.Mappings;
+
+public class AgendamentoMapping : IEntityTypeConfiguration<Agendamento>
+{
+    public void Configure(EntityTypeBuilder<Agendamento> builder)
+    {
+        builder.ToTable("agendamentos");
+
+        builder.HasKey(x => x.Id);
+
+        builder.Property(x => x.Ativo)
+            .IsRequired();
+
+        builder.HasOne(x => x.Aluno)
+            .WithMany(a => a.Agendamentos)
+            .HasForeignKey(x => x.CodigoAluno);
+
+        builder.HasOne(x => x.Aula)
+            .WithMany(a => a.Agendamentos)
+            .HasForeignKey(x => x.CodigoAula);
+
+        builder.HasIndex(x => new { x.CodigoAluno, x.CodigoAula })
+            .IsUnique();
+    }
+}
