4096f5e7ecf893a109ca1d4ab5a9e382844eee39
Antonio Elias <aelias12j@gmail.com>
2026-04-08 23:41:58 -0300

    feat : criando o contexto da aplicação

---
+++ src/FitAgenda.Data/Context/AppDbContext.cs
@@ -0,0 +1,23 @@
+﻿using FitAgenda.Domain.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace FitAgenda.Data.Context;
+
+public class AppDbContext : DbContext
+{
+    public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+    {
+    }
+
+    public DbSet<Aluno> Alunos { get; set; }
+    public DbSet<Aula> Aulas { get; set; }
+    public DbSet<TipoAula> TiposAula { get; set; }
+    public DbSet<Agendamento> Agendamentos { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.ApplyConfigurationsFromAssembly(typeof(AppDbContext).Assembly);
+        base.OnModelCreating(modelBuilder);
+    }
+
+}
